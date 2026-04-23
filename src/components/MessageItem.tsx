af30793c24f9db8c9134d0a6bcfbe2d1cd8bb27e
Lais <laiscristina.animamundi@gmail.com>
2026-04-23 19:15:18 -0300

    corecao

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -32,7 +32,7 @@ const MessageItemComponent: React.FC<MessageItemProps> = ({ item, selectedIds, s
     >
       {/* Avatar / Checkbox Logica */}
       <div className="relative w-12 h-12 shrink-0">
-        {/* Display Checkbox on Hover OR if Any Selected */}
+        {/* Display for Checkbox on Hover OR if Any Selected */}
         <div className={`
           absolute inset-0 z-10 flex items-center justify-center bg-white dark:bg-slate-800 rounded-full transition-opacity
           ${isAnySelected ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}
